b3c58b260a0db9017211a0a6ecdbe0c3515d5ee0
MatthewAPeters <matthewapeters@comcast.net>
2025-01-15 08:25:37 -0800

    debugging

--- 2024/16/cmd/aoc_16/main.go
+++ 2024/16/cmd/aoc_16/main.go
@@ -1,72 +1,17 @@
 package main
 
 import (
+	"aoc_16/pkg/edge"
+	"aoc_16/pkg/min_priority_queue"
+	"aoc_16/pkg/utils"
+
 	"container/heap"
 	"fmt"
 	"os"
 	"strings"
 )
 
-type Coord struct {
-	X int
-	Y int
-}
 
-func (c Coord)To(direction int) (Coord) {
-	return Coord{c.X + Moves[direction].X, c.Y+Moves[direction].Y}
-}
-
-func (c Coord)String()string{
-	return fmt.Sprintf("{%d,%d}", c.X, c.Y)
-}
-
-type Edge struct {
-	Location Coord
-	IncomingDirection int
-	CuumulativeCost int
-	OutGoingDirection int
-	Symbol rune
-	Index int
-	Path []*Edge
-}
-
-func (e Edge)String()string{
-	return fmt.Sprintf("%s%s%s%s[%d]%s", string(Symbols[e.IncomingDirection]),e.Location,string(Symbols[e.OutGoingDirection]), e.Location.To(e.OutGoingDirection), e.CuumulativeCost, string(e.Symbol))
-}
-
-type PriorityQueue []*Edge
-
-func (pq PriorityQueue)Len()int {
-	return len(pq)
-}
-
-func (pq PriorityQueue) Swap(i,j int) {
-	pq[i],pq[j] = pq[j],pq[i]
-	pq[i].Index = i
-	pq[j].Index = j
-}
-
-func (pq PriorityQueue)Less(i,j int) bool {
-	return pq[i].CuumulativeCost < pq[j].CuumulativeCost
-}
-
-func (pq *PriorityQueue)Push(x any){
-	n := len(*pq)
-	edge := x.(Edge)
-	edge.Index = n
-	fmt.Printf("push %s\n", edge)
-	*pq = append(*pq, &edge)
-}
-
-func (pq *PriorityQueue) Pop() any {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	old[n-1] = nil  // don't stop the GC from reclaiming the item eventually
-	item.Index = -1 // for safety
-	*pq = old[0 : n-1]
-	return item
-}
 
 const MaxCoords = 'M'
 const Wall = '#'
@@ -77,99 +22,101 @@ const OneTurn = 1000
 const OneMove = 1
 
 // set of visited cells
-var Visited = map[string]Edge{}
-
-/*
-To find the opposite of a direction, add 2 and % 4
-To find the turns, add +1 or -1 and % 4
-*/
-const (	Right = iota
-		Up
-	  	Left
-	   	Down
-		)
-
-
-var Symbols =[]rune {
-	Right: '>',
-	Up: '^',
-	Left:'<',
-	Down: 'V',
-}
-
-// Moves maps movement vectors to runes
-var Moves = map[int]Coord{
-	Up:{0,-1},
-	Down:{0,1},
-	Left: {-1,0},
-	Right: {1,0},
-}
-
-func (current Edge)LookAround()(moves []Edge){
-	// examine cells in cardinal directions from c
-	// return unvisisted spaces (or the End) as a list of Edges
-	// Edges are not costed
-	moves = []Edge{}
-
-	var partialPath = []*Edge{}
-	if len(current.Path) >= 2 {
-		partialPath = current.Path[:len(current.Path)-1]
+var Visited = map[string]edge.Edge{}
+func DrawTheMap(){
+	//   Draw the map
+	fmt.Print(" ")
+	for x := range SPECIAL['M'].X+1 {
+		fmt.Printf("%d", x%10)
 	}
+	fmt.Println()
+	for y := range SPECIAL['M'].Y+1 {
+		fmt.Printf("%d", y%10)
+		for x := range SPECIAL['M'].X+1 {
+			fmt.Printf("%s", string(MAP[utils.Coord{X:x,Y:y}.String()]))
+		}
+		fmt.Println()
+	}
+}
+
+
+
+func LookAround(current edge.Edge, pq *min_priority_queue.PriorityQueue)(moves []edge.Edge){
+	var edj *edge.Edge
+	backwards  := (current.OutGoingDirection+ 2) % 4
+	//fmt.Printf("Location: %s  Going %d (%s)  backwards is %d (%s) \n", current.Location, current.OutGoingDirection, string(utils.Symbols[current.OutGoingDirection]), backwards,string(utils.Symbols[backwards]) )
+
+	// examine cells in cardinal directions from current.Location
 	for direction := range 4 {
-		// ignore the "backwards" move
-		if (direction + 2) % 4 == current.IncomingDirection ||
-			MAP[current.Location.To(direction)] == Wall {
+		newLocation := current.Location.To(direction)
+		symbol := MAP[newLocation.String()]
+
+		// ignore the "backwards" move and ignore walls
+		if direction == backwards || symbol == Wall {
 			continue
 		}
 
-		newLocation := current.Location.To(direction)
 		cost := current.CuumulativeCost + OneMove
-		// Symbol is what we found on the MAP ...
-		if direction != current.IncomingDirection {
+		if direction != current.OutGoingDirection{
 			cost += OneTurn
-			// or what our old direction was if the direction changes
 		}
-		// clone the current Edge, but change its direction to the direction of the move
-		// its path is no longer necessary
-		clonedCurrent := Edge{current.Location, current.IncomingDirection, current.CuumulativeCost, direction, current.Symbol, current.Index, []*Edge{}}
-		// create the edge for the new move
-		edge := Edge{newLocation, direction, cost, direction, MAP[newLocation],-1,[]*Edge{}}
-		edge.Path = append(partialPath, []*Edge{&clonedCurrent, &edge}...)
 
-		if _, ok := Visited[edge.String()]; !ok &&( edge.Symbol == Space || edge.Symbol == End) {
-			moves = append(moves, edge)
-		} else {
-			if e, ok := Visited[edge.String()]; ok && cost < e.CuumulativeCost {
-				fmt.Printf("re-costing %s from %d to %d\n", e, e.CuumulativeCost, cost)
-				e.CuumulativeCost = cost
-				moves = append(moves, e)
-			}
+		e := edge.Edge{
+			Location: newLocation,
+			IncomingDirection: current.OutGoingDirection,
+			CuumulativeCost: cost,
+			OutGoingDirection: direction,
+			Symbol: symbol,
+			Index: -1,
+			Path: current.Path,
 		}
+
+		// used to check if any moves were found
+		edj = &e
+
+		if e2, ok := Visited[edj.OutVector().String()]; ok && cost < e2.CuumulativeCost {
+				fmt.Printf("re-costing %s from %d to %d\n", e2, e2.CuumulativeCost, cost)
+				delete(Visited, e2.OutVector().String())
+				heap.Push(pq, e)
+				continue
+		}
+		if e2, ok := Visited[edj.OutVector().String()]; ok {
+				fmt.Printf("DON'T LOOP AT %s \n", e2)
+				MAP[current.Location.String()]='@'
+				continue
+		}
+		if _, ok := Visited[edj.OutVector().String()]; !ok &&( e.Symbol == Space || e.Symbol == End) {
+			heap.Push(pq, e)
+		}
+	}
+	if edj == nil {
+		fmt.Printf("LOCATION %s IS DEAD END\n", current)
+		MAP[current.Location.String()]='X'
 	}
 	return
 }
 
-func Dijkstra(pq *PriorityQueue)(winner *Edge){
+func Dijkstra(pq *min_priority_queue.PriorityQueue)(winner edge.Edge){
+	// repeatedly process the lowest-cost edge until the end is found
 	for (*pq).Len()>0 {
-		var edge *Edge = heap.Pop(pq).(*Edge)
-		if edge.Location == SPECIAL[End]{
-			fmt.Printf("Found End at %s\n", edge)
-			return edge
+		var edj *edge.Edge = heap.Pop(pq).(*edge.Edge)
+		if edj.Location == SPECIAL[End]{
+			return *edj
 		}
-		// repeatedly process the lowest-cost edge until the end is found
-		for _, move := range  edge.LookAround() {
-			heap.Push(pq, move)
-			Visited[edge.String()] = move
-		}
+		// record that we have visited this location going this direction
+		Visited[edj.OutVector().String()] = *edj
+		edj.Path = append([]*(edge.Edge){}, append(edj.Path, edj)...)
+		// where can we go from here?
+		LookAround(*edj, pq)
 	}
 	return
 }
 
 
-var MAP = map[Coord]rune{}
+var MAP = map[string]rune{}
 
 // Track start and end coordinates
-var SPECIAL = map[rune]Coord{}
+var SPECIAL = map[rune]utils.Coord{}
 
 func main() {
 	fileName := os.Args[1]
@@ -182,8 +129,8 @@ func main() {
 	rawInput := strings.Split(string(raw),"\n")
 	for y,row := range rawInput{
 		for x,r := range row{
-			location := Coord{x,y}
-			MAP[location] = r
+			location := utils.Coord{X:x,Y:y}
+			MAP[location.String()] = r
 			if r == Start || r == End {
 				//fmt.Printf("SPECIAL %s at %d,%d\n",string(r), location.X,location.Y)
 				SPECIAL[r]=location
@@ -193,33 +140,22 @@ func main() {
 		}
 	}
 
-	pq := &PriorityQueue{}
-	heap.Init(pq)
-	e := Edge{SPECIAL['S'],Right,0,Right, Start,0, []*Edge{}}
-	e.Path = []*Edge{&e}
-	heap.Push(pq, e)
+	DrawTheMap()
 
+	pq := &min_priority_queue.PriorityQueue{}
+	heap.Init(pq)
+	e := edge.Edge{Location:SPECIAL['S'],IncomingDirection: utils.East,CuumulativeCost: 0,OutGoingDirection: utils.East, Symbol: Start,Index: 0, Path:[]*edge.Edge{}}
+	e.Path = append(e.Path, &e)
+	LookAround(e, pq)
 	winner := Dijkstra(pq)
 
 	// Render the map and the path taken
 	//   First, copy the path taken into the map
 	for _,e := range winner.Path {
-		MAP[e.Location] = Symbols[e.OutGoingDirection]
+		MAP[e.Location.String()] = utils.Symbols[e.OutGoingDirection]
 		fmt.Printf("%s  ", e)
 	}
 	fmt.Printf("\n\n")
-	//   Draw the map
-	fmt.Print(" ")
-	for x := range SPECIAL['M'].X+1 {
-		fmt.Printf("%d", x%10)
-	}
-	fmt.Println()
-	for y := range SPECIAL['M'].Y+1 {
-		fmt.Printf("%d", y%10)
-		for x := range SPECIAL['M'].X+1 {
-			fmt.Printf("%s", string(MAP[Coord{x,y}]))
-		}
-		fmt.Println()
-	}
+	DrawTheMap()
 	fmt.Printf("\nThe winning score is %d\n", winner.CuumulativeCost)
 }
